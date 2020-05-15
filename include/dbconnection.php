c1760f5c3601f4996f3af8d4c1576e216a8ea391
AdAli92 <48913117+AdAli92@users.noreply.github.com>
2020-05-15 04:35:31 +0200

    Update dbconnection.php

--- include/dbconnection.php
+++ include/dbconnection.php
@@ -8,8 +8,8 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 error_reporting(0);
 
 $host = "maxscale";
-$user = "??????"; // change to your ALTO user
-$passwd = "??????"; // change to your ALTO password
+$user = "dats25"; // change to your ALTO user
+$passwd = "express much room"; // change to your ALTO password
 $db = "studentinfo";
 
 
